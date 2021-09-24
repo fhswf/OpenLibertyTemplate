504d86d4cbd3ecf05401404f87fd09d1b991cee3
Christian Gawron <gawron.christian@fh-swf.de>
2021-09-24 14:52:53 +0200

    create example servlet

---
+++ src/main/java/de/fhswf/ResponseServlet.java
@@ -0,0 +1,32 @@
+package de.fhswf.fit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/response")
+public class ResponseServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            String username = request.getParameter("username");
+            if (username != null && username.length() > 0) {
+                out.println("<h2>Hello, " + username + "!</h2>");
+            }
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "The Response servlet says hello.";
+
+    }
+}
---
+++ src/main/java/de/fhswf/WelcomeServlet.java
@@ -0,0 +1,59 @@
+package de.fhswf.fit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * This is a simple example of an HTTP Servlet. It responds to the GET method of
+ * the HTTP protocol.
+ */
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setBufferSize(8192);
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html lang=\"en\">"
+                    + "<head><title>Servlet Hello</title></head>");
+            
+            // then write the data of the response
+            out.println("<body  bgcolor=\"#ffffff\">"
+                    + "<form method=\"get\">"
+                    + "<h2>Hallo, ich bin Christian. Wie ist Dein Name?</h2>"
+                    + "<input title=\"Ich heiße ...\" type=\"text\" "
+                    + "name=\"username\" size=\"25\"/>"
+                    + "<p></p>"
+                    + "<input type=\"submit\" value=\"Submit\"/>"
+                    + "<input type=\"reset\" value=\"Reset\"/>"
+                    + "</form>");
+            
+            String username = request.getParameter("username");
+            if (username != null && username.length() > 0) {
+                RequestDispatcher dispatcher =
+                        getServletContext().getRequestDispatcher("/response");
+                
+                if (dispatcher != null) {
+                    dispatcher.include(request, response);
+                }
+            }
+            out.println("</body></html>");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "The Hello servlet says hello.";
+
+    }
+}
